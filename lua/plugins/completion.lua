0f7b978fa3185af2237aff245f1bfaded9f3ec44
my66 <my6666@gmail.com>
2026-03-08 20:48:19 +0800

    feat(cmp): 增强 2026 实用型补全体验

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -2,59 +2,89 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
     },
     config = function()
-      -- 补全菜单行为：弹出但不自动插入或选中
       vim.opt.completeopt = { "menu", "menuone", "noselect", "noinsert" }
-      -- 半透明弹出框，数值越大越透明
 
       local cmp = require("cmp")
       local luasnip = require("luasnip")
       local win = require("cmp.config.window")
 
+      require("luasnip.loaders.from_vscode").lazy_load()
+
       cmp.setup({
+        preselect = cmp.PreselectMode.None,
+        performance = {
+          debounce = 25,
+          throttle = 20,
+          fetching_timeout = 150,
+        },
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
           end,
         },
         completion = {
-          --autocomplete = { cmp.TriggerEvent.TextChanged },
-          --autocomplete = { cmp.TriggerEvent.InsertEnter, cmp.TriggerEvent.TextChanged }
-          autocomplete = false
+          autocomplete = {
+            cmp.TriggerEvent.InsertEnter,
+            cmp.TriggerEvent.TextChanged,
+          },
+          completeopt = "menu,menuone,noselect,noinsert",
+          keyword_length = 1,
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-k>"] = cmp.mapping.complete(),
-          ["<CR>"]      = cmp.mapping.confirm({ select = false }),
-          -- 使用 cmp 自身的行为插入并选择
-          ["<C-n>"]     = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"]     = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-e>"] = cmp.mapping.abort(),
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
             elseif luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
             else
               fallback()
             end
-          end, { 'i', 's' }),
+          end, { "i", "s" }),
           ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
             elseif luasnip.jumpable(-1) then
               luasnip.jump(-1)
             else
               fallback()
             end
-          end, { 'i', 's' }),
+          end, { "i", "s" }),
         }),
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip"    },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp", priority = 1000 },
+          { name = "luasnip", priority = 750 },
+          { name = "path", priority = 500 },
+          { name = "buffer", priority = 250, keyword_length = 3 },
+        }),
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            cmp.config.compare.kind,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
         },
         experimental = {
           ghost_text = true,
@@ -69,8 +99,23 @@ return {
         },
       })
 
-      -- 自定义补全菜单和文档的高亮
-      vim.cmd([[ 
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+
+      vim.cmd([[
         highlight CmpPmenu      guibg=#1e222a guifg=#c5c8c6
         highlight CmpPmenuSel   guibg=#3e4452 guifg=#ffffff
         highlight CmpDoc        guibg=#22272e guifg=#abb2bf
@@ -81,4 +126,3 @@ return {
     end,
   },
 }
-
